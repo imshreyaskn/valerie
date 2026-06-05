742fa12ae51ee6c80c7ea3c3530d556678a9733e
Shreyas <oyedrizzle@gmail.com>
2026-06-05 17:22:52 +0530

    style: add giant version watermark to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,24 +2,25 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <footer className="footer-metadata">
-      <div>
-        designed & engineered by<br/>
-        shreyas k n
+    <>
+      <footer className="footer-metadata">
+        <div>
+          designed & engineered by<br/>
+          shreyas k n
+        </div>
+        <div>
+          <a href="https://linkedin.com/in/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">linkedin</a><br/>
+          <a href="https://github.com/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">github</a><br/>
+          <a href="mailto:imshreyaskn@gmail.com" className="footer-link">imshreyaskn@gmail.com</a>
+        </div>
+        <div>
+          updated on<br/>
+          06/05/2026
+        </div>
+      </footer>
+      <div className="massive-version">
+        v.0.1.1
       </div>
-      <div>
-        <a href="https://linkedin.com/in/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">linkedin</a><br/>
-        <a href="https://github.com/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">github</a><br/>
-        <a href="mailto:imshreyaskn@gmail.com" className="footer-link">imshreyaskn@gmail.com</a>
-      </div>
-      <div>
-        updated on<br/>
-        06/05/2026
-      </div>
-      <div>
-        VALERIE<br/>
-        <strong>v0.1.0</strong>
-      </div>
-    </footer>
+    </>
   );
 }
