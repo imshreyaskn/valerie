1e294100fc543fc53aae1be5e73d6d9d1d62ac73
Shreyas <oyedrizzle@gmail.com>
2026-06-05 17:33:46 +0530

    style: resize footer version tag and restore 4-col grid

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,25 +2,23 @@ import './Footer.css';
 
 export default function Footer() {
   return (
-    <>
-      <footer className="footer-metadata">
-        <div>
-          designed & engineered by<br/>
-          shreyas k n
-        </div>
-        <div>
-          <a href="https://linkedin.com/in/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">linkedin</a><br/>
-          <a href="https://github.com/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">github</a><br/>
-          <a href="mailto:imshreyaskn@gmail.com" className="footer-link">imshreyaskn@gmail.com</a>
-        </div>
-        <div>
-          updated on<br/>
-          06/05/2026
-        </div>
-      </footer>
-      <div className="massive-version">
+    <footer className="footer-metadata">
+      <div>
+        designed & engineered by<br/>
+        shreyas k n
+      </div>
+      <div>
+        <a href="https://linkedin.com/in/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">linkedin</a><br/>
+        <a href="https://github.com/imshreyaskn" target="_blank" rel="noreferrer" className="footer-link">github</a><br/>
+        <a href="mailto:imshreyaskn@gmail.com" className="footer-link">imshreyaskn@gmail.com</a>
+      </div>
+      <div>
+        updated on<br/>
+        06/05/2026
+      </div>
+      <div className="footer-version-tag">
         v.0.1.1
       </div>
-    </>
+    </footer>
   );
 }
